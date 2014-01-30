5da45d42811538a418b7df745b0c210f8c27f5f8
Mateusz Bysiek <mb@mbdev.pl>
2014-01-31 00:08:52 +0100

    removed useless usings
    Change-Id: Ia1b1d294a591b46aad5808d68bde892e7080c0a2
    Signed-off-by: Mateusz Bysiek <mb at mbdev.pl>

--- src/MBdevExtensions/PresentationExtensions.cs
+++ src/MBdevExtensions/PresentationExtensions.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
+﻿using System.ComponentModel;
+using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
